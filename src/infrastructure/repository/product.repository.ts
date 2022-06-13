37b1d7d61289532030e3339d02064a7aa89a7f65
Lucas <lucas@MacBook-Air-de-Lucas.local>
2022-06-13 19:55:53 -0300

    feat: implement tests and methods from create and update products

--- src/infrastructure/repository/product.repository.ts
+++ src/infrastructure/repository/product.repository.ts
@@ -4,7 +4,9 @@ import { ProductModel } from "../db/sequelize/model/product.model";
 
 export class ProductRepository implements ProductRepositoryInterface {
   async find(id: string): Promise<Product> {
-    throw new Error("Method not implemented.");
+    const productModel = await ProductModel.findOne({ where: { id } });
+
+    return new Product(productModel.id, productModel.name, productModel.price);
   }
   async findAll(): Promise<Product[]> {
     throw new Error("Method not implemented.");
