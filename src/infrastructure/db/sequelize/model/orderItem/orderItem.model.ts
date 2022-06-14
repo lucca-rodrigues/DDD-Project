b06da857c3123ebe12c662bf649377ff0503b454
Lucas <lucas@MacBook-Air-de-Lucas.local>
2022-06-14 19:26:36 -0300

    feat: implement infrastructure model order item

--- src/infrastructure/db/sequelize/model/orderItem/orderItem.model.ts
+++ src/infrastructure/db/sequelize/model/orderItem/orderItem.model.ts
@@ -1,18 +1,47 @@
-import { Table, Model, PrimaryKey, Column } from "sequelize-typescript";
+import {
+  Table,
+  Model,
+  PrimaryKey,
+  Column,
+  ForeignKey,
+  BelongsTo,
+} from "sequelize-typescript";
+import { CustomerModel } from "../customer/customer.model";
+import { ProductModel } from "../product/product.model";
 
 @Table({
-  tableName: "products",
+  tableName: "order_items",
   timestamps: false,
 })
-export class ProductModel extends Model {
+export class OrderItemModel extends Model {
   @PrimaryKey
   @Column
   declare id: string;
 
+  // Relationship
+  @ForeignKey(() => ProductModel)
+  @Column({ allowNull: false })
+  declare product_id: string;
+
+  @BelongsTo(() => ProductModel)
+  declare product: ProductModel;
+
+  @ForeignKey(() => ProductModel)
+  @Column({ allowNull: false })
+  declare order_id: string;
+
+  @BelongsTo(() => ProductModel)
+  declare order: ProductModel;
+
   @Column({
     allowNull: false,
   })
-  declare name: string;
+  declare quantity: number;
+
+  @Column({
+    allowNull: false,
+  })
+  declare name: number;
 
   @Column({
     allowNull: false,
